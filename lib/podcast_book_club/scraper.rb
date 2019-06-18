c3f623a639f5685f852c1e710598637f598dda16
Jess Rezac <jess.rezac@gmail.com>
2019-06-18 16:23:18 +0000

    build #fetch_episodes scraper

--- lib/podcast_book_club/scraper.rb
+++ lib/podcast_book_club/scraper.rb
@@ -1,13 +1,27 @@
-require "nokogiri"
-require "open-uri"
-require "pry"
+require_relative "../podcast_book_club.rb"
 
 class Scraper
     def initialize
         puts "hello, where should i scrape?"
-        input = gets.chomp
+        input = "https://player.fm/series/the-ezra-klein-show"
         fetch_episodes(input)
         write_books(episodes)
+    end
+    
+    def fetch_episodes(path)
+      html = open(path)
+      doc = Nokogiri::HTML(html)
+      
+      episodes = doc.css(".info")
+      
+      episodes.each do |episode|
+        title = 
+        link = 
+        
+        binding.pry
+        
+        Episode.new
+      end
     end
 
     def fetch_episodes(path)
