c23bb3daef8103b4d83e75679f7374279e4c3aad
Jess Rezac <jess.rezac@gmail.com>
2019-06-18 22:23:00 -0500

    build book class

--- lib/podcast_book_club.rb
+++ lib/podcast_book_club.rb
@@ -5,3 +5,4 @@ require "pry"
 require_relative './podcast_book_club/version.rb'
 require_relative './podcast_book_club/cli.rb'
 require_relative './podcast_book_club/episode.rb'
+require_relative './podcast_book_club/book.rb'
