7624504b0df91a9631f993ec210dfdd8e1127a76
Jess Rezac <jess.rezac@gmail.com>
2019-06-21 16:40:21 -0500

    add explicit config environment

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require "pry"
+require "nokogiri"
+require "open-uri"
+
+require_relative '../lib/podcast_book_club/version'
+require_relative '../lib/podcast_book_club/cli'
+require_relative '../lib/podcast_book_club/episode'
+require_relative '../lib/podcast_book_club/book'
--- lib/podcast_book_club.rb
+++ lib/podcast_book_club.rb
@@ -1,8 +1,4 @@
-require "nokogiri"
-require "open-uri"
-require "pry"
+module PodcastBookClub
+end
 
-require_relative './podcast_book_club/version.rb'
-require_relative './podcast_book_club/cli.rb'
-require_relative './podcast_book_club/episode.rb'
-require_relative './podcast_book_club/book.rb'
+require_relative '../config/environment'
