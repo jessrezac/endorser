027ed465df5b96dfd0c386f7c17f289484cf6203
Jess Rezac <jess.rezac@gmail.com>
2019-06-24 20:44:01 +0000

    create #build_path

--- lib/podcast_book_club/scraper.rb
+++ lib/podcast_book_club/scraper.rb
@@ -7,19 +7,23 @@ class Scraper
         fetch_episodes(input)
         write_books(Episode.all)
     end
-    
+
+    def build_path
+        url = "https://player.fm/series/the-ezra-klein-show/episodes?active=true&limit=1000&order=newest&query=&style=list&container=false&offset=0"
+    end
+
     def fetch_episodes(path)
       html = open(path)
       doc = Nokogiri::HTML(html)
-      
+
       episodes = doc.css(".info-top")
-      
+
       episodes.each do |episode|
         title = episode.css("a").text.strip
         link = "https://player.fm#{episode.css("a").attribute("href").value}"
-        
+
         attributes = {title: title, link: link}
-        
+
         Episode.new(attributes)
 
       end
@@ -28,15 +32,15 @@ class Scraper
     def write_books(episodes)
         episodes.each do |episode|
           path = episode.link
-          
+
           html = open(path)
           doc = Nokogiri::HTML(html)
-          
+
           description_links = doc.css(".description.prose>strong~a")
 
           description_links.each do |link|
             book_path = link.attribute("href")
-            
+
             Book.new_from_url(book_path)
 
           end
