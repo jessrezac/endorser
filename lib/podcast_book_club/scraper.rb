d957ce78664626da6be1b6120b2301bfc75df488
Jess Rezac <jess.rezac@gmail.com>
2019-06-26 19:25:36 +0000

    create Scraper#build_path to increment URL to download new episodes over time

--- lib/podcast_book_club/scraper.rb
+++ lib/podcast_book_club/scraper.rb
@@ -3,15 +3,9 @@ require_relative "../podcast_book_club.rb"
 class Scraper
     def initialize
         puts "hello, where should i scrape?"
-        input = "https://player.fm/series/the-ezra-klein-show"
-        build_path
-        fetch_episodes(input)
+        path = build_path
+        fetch_episodes(path)
         write_books(Episode.all)
-    end
-
-    def build_path
-        binding.pry
-        url = "https://player.fm/series/the-ezra-klein-show/episodes?active=true&limit=1000&order=newest&query=&style=list&container=false&offset=0"
     end
 
     def fetch_episodes(path)
@@ -23,6 +17,7 @@ class Scraper
       episodes.each do |episode|
         title = episode.css("a").text.strip
         link = "https://player.fm#{episode.css("a").attribute("href").value}"
+
 
         attributes = {title: title, link: link}
 
@@ -49,6 +44,15 @@ class Scraper
 
         end
 
+    end
+
+    private
+
+    def build_path
+        snapshot_date = Date.new(2019,6,25)
+        today = Date.today
+        episodes_since_snapshot = snapshot_date.step(today).select{|d| d.monday? || d.wednesday?}.size
+        url = "https://player.fm/series/the-ezra-klein-show/episodes?active=true&limit=#{episodes_since_snapshot + 225}&order=newest&query=&style=list&container=false&offset=0"
     end
 
   end
