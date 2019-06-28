d8b62462fc817746adfc7892529d9832104be874
Jess Rezac <jess.rezac@gmail.com>
2019-06-28 14:32:22 -0500

    write PodcastBookClub::CLI.call

--- lib/podcast_book_club/cli.rb
+++ lib/podcast_book_club/cli.rb
@@ -1,8 +1,26 @@
 class PodcastBookClub::CLI
 
+    def initialize
+        call
+    end
+
     def call
-        puts "Welcome to Podcast Book Club"
+        puts "Welcome to Podcast Book Club!"
+        puts "The Ezra Klein Show brings you far-reaching conversations about hard problems, big ideas,"
+        puts "illuminating theories, and cutting-edge research."
+        puts "Podcast Book Club lets you climb around the guests' bookshelves."
+        puts "Let's get started!"
+        puts ""
+        puts "Loading episodes..."
+        puts ""
         Scraper.new
+        puts "To list available episodes to build a library, enter 'list episodes'."
+        puts "To create a library from all episodes, enter 'create library'."
+        puts ""
+        puts "What would you like to do?"
+        input = gets.chomp
+        binding.pry 
+
     end
 
 end
