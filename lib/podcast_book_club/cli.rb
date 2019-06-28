33c17e8cc0bf76f597ba6815fce86e59197006c4
Jess Rezac <jess.rezac@gmail.com>
2019-06-28 15:21:11 -0500

    continue PodcastBookClub::CLI.call

--- lib/podcast_book_club/cli.rb
+++ lib/podcast_book_club/cli.rb
@@ -13,13 +13,31 @@ class PodcastBookClub::CLI
         puts ""
         puts "Loading episodes..."
         puts ""
-        Scraper.new
+        @scraper = Scraper.new
         puts "To list available episodes to build a library, enter 'list episodes'."
         puts "To create a library from all episodes, enter 'create library'."
         puts ""
         puts "What would you like to do?"
-        input = gets.chomp
-        binding.pry 
+        input = gets.chomp.downcase
+
+        until input == "exit"
+
+            case input
+            when "list episodes"
+                Episode.all.map {|episode| "#{episode.title}"}
+            when "create library"
+                Episode.all.each do |episode|
+                    @scraper.build_books(episode)
+                end
+
+                binding.pry
+            else
+                puts "Sorry, I did not understand your response."
+                puts "What would you like to do?"
+                input = gets.chomp.downcase
+            end
+
+        end
 
     end
 
