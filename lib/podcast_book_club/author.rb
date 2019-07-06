12371d3e3247f68a78ffac221b997568edf0c148
Jess Rezac <jess.rezac@gmail.com>
2019-07-06 17:55:25 -0500

    create author and genre classes

---
+++ lib/podcast_book_club/author.rb
@@ -0,0 +1,22 @@
+class Author
+    attr_accessor :name, :books
+  
+    extend Memorable::ClassMethods
+    extend Findable::ClassMethods
+    include Memorable::InstanceMethods
+  
+    @@all = []
+  
+    def initialize(name)
+      
+      @name = name
+      @books = []
+      
+  
+    end
+  
+    def self.all
+      @@all
+    end
+  
+  end
